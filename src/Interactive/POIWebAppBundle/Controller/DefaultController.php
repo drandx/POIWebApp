c5687d41b3205a1cf9f2ee95032cacb978d5dc48
drandx <garjuanpablo@gmail.com>
2014-03-01 22:39:47 -0600

    Login and User providers from db working.
    Signed-off-by: drandx <garjuanpablo@gmail.com>

--- src/Interactive/POIWebAppBundle/Controller/DefaultController.php
+++ src/Interactive/POIWebAppBundle/Controller/DefaultController.php
@@ -1,8 +1,7 @@
 <?php
-
 namespace Interactive\POIWebAppBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
@@ -10,4 +9,25 @@ class DefaultController extends Controller
     {
         return $this->render('POIWebAppBundle:Default:index.html.twig', array('name' => $name));
     }
+    
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+ 
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+ 
+        return $this->render('POIWebAppBundle:Default:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+    
 }
