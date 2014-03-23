d8180280300736cba2a8f8fe22d732cd52e4cd24
drandx <garjuanpablo@gmail.com>
2014-03-23 12:56:13 -0500

    Created Category CRUD
    Signed-off-by: drandx <garjuanpablo@gmail.com>

---
+++ src/Interactive/POIWebAppBundle/Form/CategoryType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Interactive\POIWebAppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CategoryType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('slug')
+            ->add('affiliates')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Interactive\POIWebAppBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'interactive_poiwebappbundle_category';
+    }
+}
