039e30cca3b601cf27d2f6cab53cb893cce8c50b
drandx <garjuanpablo@gmail.com>
2014-03-27 11:39:20 -0500

    Reverted GEO creation
    Signed-off-by: drandx <garjuanpablo@gmail.com>

---
+++ src/Interactive/POIWebAppBundle/Form/GeoStateType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Interactive\POIWebAppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GeoStateType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('idPais')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Interactive\POIWebAppBundle\Entity\GeoState'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'interactive_poiwebappbundle_geostate';
+    }
+}
